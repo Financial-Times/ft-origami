ea4fdebec5f1bb617c1fbad8362db867a42d5314
Andrew Betts <andrew.betts@ft.com>
2013-10-12 20:26:18 +0100

    Better responsiveness on tables and headings

--- js/pagenav.js
+++ js/pagenav.js
@@ -1,24 +1,34 @@
+/**
+ * Add a second navigation menu to quickly navigate to
+ * anchors in the page.
+ */
+
 $(function() {
 	var list = '', lis = [], scrollmargin, scrolltimer, resizetimer;
+
+	// Find heading 2s and build a link list.  Only proceed if there would be more than 1 item in the list
 	$('.main h2[id]').each(function() {
 		lis.push('<li id="pagenav-'+this.id+'"><a href="#'+this.id+'">'+this.innerText+'</a></li>');
 	});
 	if (lis.length < 2) return;
-
 	list = '<ul class="nav sidebar" id="pagenav">'+lis.join('')+'</ul>';
+
+	// Insert the new nav list after the existing one
 	$('#sitenav').after(list);
 
+	// Turn it into a bootstrap 'affix' component, which will keep it in view as the user scrolls
 	$('#pagenav').affix({
 		offset: { top: $('#pagenav').offset().top-50 }
 	});
 
+	// Determine border tolerance for highlighting nav sections (once immediately, and then on resize)
 	calcScrollMargin();
-
 
 	function calcScrollMargin() {
 		scrollmargin = $(window).height() / 5;
 	}
 
+	// On scroll, determine which section is in view, and highlight it
 	$(document).on('scroll', function() {
 		if (scrolltimer) return;
 		scrolltimer = setTimeout(function() {
@@ -42,6 +52,7 @@ $(function() {
 		}, 300);
 	});
 
+	// On resize, reconsider boundary tolerance for section highlighting
 	$(window).on('resize', function() {
 		if (resizetimer) return;
 		resizetimer = setTimeout(function() {
---
+++ js/tablewrap.js
@@ -0,0 +1,7 @@
+/**
+ * Wrap tables so that they scroll horizontally on small screens
+ */
+
+$(function() {
+	$('.main table.table').wrap('<div class="table-wrap"></div>');
+});
